b884ddad856871aa305513f2529fc38c2ea171eb
Christos <pollalis.ch@gmail.com>
2016-10-12 13:33:33 +0300

    Re-code the scale workflow to take into account the changes introduced by `kubeadm`
    Also, an attempt to get the `master_token` from the runtime_properties
    of the Kubernetes Master

--- workflows/scale_kubernetes.py
+++ workflows/scale_kubernetes.py
@@ -9,42 +9,48 @@ import pkg_resources
 import glob
 import requests
 
-from time import sleep
+import string
+import random
+
+from time import time, sleep
 
 try:
-    import connection  # FIXME
+    import connection
 except ImportError:
-    sys.path.insert(0, 'lib/python2.7/site-packages/plugin/')
+    sys.path.insert(0, 'lib/python2.7/site-packages/plugin/')  # TODO
     import connection
 
+# FIXME
+resource_package = __name__
 
-resource_package = __name__  # Could be any module/package name.
 try:
-    resource_path = os.path.join('../scripts', 'worker.sh')
-    install_worker_script = pkg_resources.resource_string(resource_package, resource_path)
-    resource_path = os.path.join('../scripts', 'master.sh')
-    install_master_script = pkg_resources.resource_string(resource_package, resource_path)
-    resource_path = os.path.join('../scripts', 'coreos_master.sh')
-    install_coreos_master_script = pkg_resources.resource_string(resource_package, resource_path)
-    resource_path = os.path.join('../scripts', 'coreos_worker.sh')
-    install_coreos_worker_script = pkg_resources.resource_string(resource_package, resource_path)
+    # This path is for `cfy local` executions
+    resource_path = os.path.join('../scripts', 'mega-deploy.sh')
+    kubernetes_script = \
+        pkg_resources.resource_string(resource_package, resource_path)
 except IOError:
+    # This path is for executions performed by Mist.io
     tmp_dir = os.path.join('/tmp/templates',
                            'mistio-kubernetes-blueprint-[A-Za-z0-9]*',
                            'scripts')
     scripts_dir = glob.glob(tmp_dir)[0]
-    resource_path = os.path.join(scripts_dir, 'worker.sh')
+    resource_path = os.path.join(scripts_dir, 'mega-deploy.sh')
     with open(resource_path) as f:
-        install_worker_script = f.read()
-    resource_path = os.path.join(scripts_dir, 'master.sh')
-    with open(resource_path) as f:
-        install_master_script = f.read()
-    resource_path = os.path.join(scripts_dir, 'coreos_master.sh')
-    with open(resource_path) as f:
-        install_coreos_master_script = f.read()
-    resource_path = os.path.join(scripts_dir, 'coreos_worker.sh')
-    with open(resource_path) as f:
-        install_coreos_worker_script = f.read()
+        kubernetes_script = f.read()
+
+
+#KUBE_TYPE = 'worker'
+CREATE_TIMEOUT = 60 * 5
+SCRIPT_TIMEOUT = 60 * 30
+
+
+def random_name(length=4):
+
+    def random_chars(length):
+        _chars = string.letters + string.digits
+        return ''.join(random.choice(_chars) for _ in range(4))
+
+    return 'MistCfyNode-%s-%s' % (random_chars(length), random_chars(length))
 
 
 def scale_cluster():
@@ -53,184 +59,185 @@ def scale_cluster():
         delta = int(delta)
 
     if delta == 0:
-        workctx.logger.info('Delta parameter equals 0! No scaling will take '
-                            'place')
+        workctx.logger.debug('Delta parameter equals 0! No scaling will take '
+                             'place')
         return
     elif delta > 0:
-        workctx.logger.info('Scaling kubernetes cluster up by {0} minion '
-                            'node(s)'.format(delta))
+        workctx.logger.info('Scaling Kubernetes cluster up by %s node(s)',
+                            delta)
         scale_cluster_up(delta)
     elif delta < 0:
         # TODO verify that (current number of nodes) - (delta) > 0
-        workctx.logger.info('Scaling kubernetes cluster down by {0} minion '
-                            'node(s)'.format(abs(delta)))
+        workctx.logger.info('Scaling Kubernetes cluster down by %s node(s)',
+                            abs(delta))
         scale_cluster_down(abs(delta))
 
 
 def scale_cluster_up(delta):
-    master = workctx.get_node("kube_master")
+    master = workctx.get_node('kube_master')
     master_instance = [instance for instance in master.instances][0]
+    # TODO deprecate this! /
     mist_client = connection.MistConnectionClient(properties=master.properties)
     client = mist_client.client
     cloud = mist_client.cloud
     master_machine = mist_client.machine
-    master_ip = master_machine.info["private_ips"][0]
+    # Private IP of the Kubernetes Master
+    master_ip = master_machine.info['private_ips'][0]
+    # /deprecate
 
     if inputs['use_external_resource']:
         machine = mist_client.other_machine(inputs)  # FIXME
 
-    machine_name = inputs["worker_name"]
+    # Name of the new Kubernetes Worker
+    machine_name = inputs.get('worker_name', '') or random_name()
     machines = cloud.machines(search=machine_name)
     if len(machines):
         for m in machines:
-            if m.info["state"] in ["running", "stopped"]:
-                raise NonRecoverableError(
-                    "Machine with name {0} exists".format(machine_name))
+            if m.info['state'] in ['running', 'stopped']:
+                workctx.log.warn('Machine \'%s\' already exists. Will try to '
+                                 'create anyway', machine_name)
 
-    key = ""
-    if inputs.get("mist_key"):
-        key = client.keys(search=inputs["mist_key"])
+    # FIXME improve this!
+    # This should not raise a NonRecoverableError. Instead, we should allow
+    # users to add a key and use it
+    if inputs.get('mist_key'):
+        key = client.keys(search=inputs['mist_key'])
         if len(key):
             key = key[0]
         else:
-            raise NonRecoverableError("key not found")
+            raise NonRecoverableError('Key not found')
     else:
-        raise NonRecoverableError("key not found")
+        raise NonRecoverableError('No key provided')
 
-    image_id = inputs.get('mist_image', '')
-    if not image_id:
-        raise NonRecoverableError('No image ID provided')
-
-    size_id = inputs.get('mist_size', '')
-    if not size_id:
-        raise NonRecoverableError('No size ID provided')
-
-    location_id = inputs.get('mist_location', '')
-    if not location_id:
-        raise NonRecoverableError('No location ID provided')
-
+    # TODO is this correct?
+    for param in ['mist_image', 'mist_size', 'mist_location']:
+        if not inputs.get(param):
+            raise NonRecovarableError('Input parameter \'%s\' is required, but '
+                                      'it is missing', param)
     networks = inputs.get('networks', [])
 
-    workctx.logger.info("Deploying %d '%s' minion node(s)", delta, machine_name)
+    workctx.logger.info("Deploying %d '%s' node(s)", delta, machine_name)
     quantity = delta
-    job_id = cloud.create_machine(async=True, name=machine_name, key=key,
-                                  image_id=image_id, location_id=location_id,
-                                  size_id=size_id, quantity=quantity,
-                                  networks=networks)
+    job = cloud.create_machine(async=True, name=machine_name, key=key,
+                               image_id=image_id, location_id=location_id,
+                               size_id=size_id, quantity=quantity,
+                               networks=networks)
 
-    job_id = job_id.json()["job_id"]
+    job_id = job.json()['job_id']
     job = client.get_job(job_id)
-    timer = 0
+    started_at = time()
+
+    workctx.logger.info('Machine creation succeeded. Probing...')
     while True:
-        if job["summary"]["probe"]["success"]:
+        if job['summary']['probe']['success']:
+            workctx.logger.info('Machine probed successfully')
             break
-        if job["summary"]["create"]["error"] or job["summary"]["probe"]["error"]:
-            workctx.logger.error('Error on machine creation:{0}'.format(job))
-            raise NonRecoverableError("Not able to create machine")
-        sleep(10)
+        if job['summary']['create']['error'] or \
+            job['summary']['probe']['error']:
+            # FIXME `job` is too much of an output
+            workctx.logger.error('Error during machine creation: %s', job)
+            raise NonRecoverableError('Machine creation failed')
+
+        if time() > started_at + CREATE_TIMEOUT:
+            # TODO print something!
+            raise NonRecoverableError('Machine creation is taking too long! '
+                                      'Backing away...')
+
+        workctx.logger.info('Waiting for machine to become responsive...')
+        sleep(5)
         job = client.get_job(job_id)
-        timer += 1
-        if timer >= 360:
-            raise NonRecoverableError("Timed-out! Not able to create machine")
 
-    kub_type = "worker"
+    ctx.logger.debug('************* %s', job)  # TODO REMOVE THIS
 
-    if not inputs["coreos"]:
-        script = """#!/bin/sh
-        command_exists() {
-        command -v "$@" > /dev/null 2>&1
-        }
-        if command_exists curl; then
-        curl -sSL https://get.docker.com/ | sh
-        elif command_exists wget; then
-        wget -qO- https://get.docker.com/ | sh
-        fi
-        """
-        response = client.add_script(name="install_docker" + kub_type + uuid.uuid1().hex,
-                                     script=script, location_type="inline",
-                                     exec_type="executable")
-        script_id = response['id']
-        cloud_id = inputs['mist_cloud']
-
-        machine_ids = []
-        for i in xrange(quantity):
-            machine_ids.append(job['logs'][2+i]['machine_id'])
-        if not machine_ids:
-            raise NonRecoverableError('Could not retrieve machine IDs')
-        workctx.logger.info("Docker installation started")
-        for machine_id in machine_ids:
-            job_id = client.run_script(script_id=script_id, cloud_id=cloud_id,
-                                       machine_id=machine_id, script_params="",
-                                       su=False)
-            job_id = job_id["job_id"]
-            job = client.get_job(job_id)
-            while True:
-                if job["error"]:
-                    raise NonRecoverableError("Not able to install docker")
-                if job["finished_at"]:
-                    break
-                sleep(10)
-                job = client.get_job(job_id)
-            workctx.logger.info(job["logs"][2]['stdout'])
-            workctx.logger.info(job["logs"][2]['extra_output'])
-            workctx.logger.info("Docker installation script succeeded")
-
-    if inputs["coreos"]:
-        install_script = install_coreos_worker_script
-    else:
-        install_script = install_worker_script
-    response = client.add_script(name="install_kubernetes_worker" + uuid.uuid1().hex,
-                                 script=install_script, location_type="inline",
-                                 exec_type="executable")
+    # FIXME re-uploading Kubernetes script
+    script_name = 'install_kubernetes_%s' % uuid.uuid1().hex
+    script = client.add_script(name=script_name, script=kubernetes_script,
+                               location_type='inline', exec_type='executable')
 
     for m in xrange(quantity):
-        machine_name = machine_name.rsplit('-', 1)[0] if quantity > 1 else machine_name
+        machine_name = \
+            machine_name.rsplit('-', 1)[0] if quantity > 1 else machine_name
         machine_name += '-' + str(m + 1) if quantity > 1 else ''
+
         inputs['name'] = machine_name
+        # FIXME `other_machine` must be ERADICATED!
         machine = mist_client.other_machine(inputs)
         inputs['machine_id'] = machine.info['id']
-        workctx.logger.info('Machine created')
 
         machine_id = inputs['machine_id']
         cloud_id = inputs['mist_cloud']
-        script_params = "-m '{0}'".format(master_ip)
-        script_id = response['id']
-        job_id = client.run_script(script_id=script_id, cloud_id=cloud_id,
-                                   machine_id=machine_id,
-                                   script_params=script_params, su=True)
-        workctx.logger.info("Kubernetes worker installation started for "
-                            "{0}".format(machine_name))
-        job_id = job_id["job_id"]
+
+        # NOTE TEST THIS # # # #
+        master_token = inputs['master_token']
+        workctx.logger.debug('************** %s', master_instance)
+        # # # # # # # # # # # #
+
+        script_params = "-m '%s' -r 'node' -t '%s'" % (master_ip, master_token)
+        script_id = script['id']
+        workctx.logger.info('Kubernetes Worker installation started for %s',
+                            machine_name)
+        script_job = client.run_script(script_id=script_id, cloud_id=cloud_id,
+                                       machine_id=machine_id,
+                                       script_params=script_params, su=True)
+
+        job_id = script_job['job_id']
         job = client.get_job(job_id)
+        started_at = job['started_at']
+
         while True:
-            if job["error"]:
-                raise NonRecoverableError("Not able to install kubernetes worker")
-            if job["finished_at"]:
+            if job['error']:
+                _stdout = job['logs'][2]['stdout']
+                _extra_stdout = job['logs'][2]['extra_output']
+                _stdout += _extra_stdout if _extra_stdout else ''
+                workctx.logger.error(_stdout)
+                raise NonRecoverableError('Installation of Kubernetes failed')
+            if time() > started_at + SCRIPT_TIMEOUT:
+#                _stdout = job['logs'][2]['stdout']
+#                _extra_stdout = job['logs'][2]['extra_output']
+#                _stdout += _extra_stdout if _extra_stdout else ''
+#                workctx.logger.debug(_stdout)
+                raise NonRecoverableError('Installation of Kubernetes is '
+                                          'taking too long! Giving up...')
+            if job['finished_at']:
                 break
-            sleep(10)
+
+            workctx.logger.info('Waiting for Kubernetes to be installed...')
+            sleep(5)
             job = client.get_job(job_id)
-        master_instance.execute_operation('cloudify.interfaces.lifecycle.associate',
-                                          kwargs={'minion_id': machine_id})
-        workctx.logger.info(job["logs"][2]['stdout'])
-        workctx.logger.info(job["logs"][2]['extra_output'])
-        workctx.logger.info("Kubernetes worker {0} installation script succeeded".format(inputs["name"]))
-    workctx.logger.info("Upscaling kubernetes cluster succeeded")
+
+        workctx.logger.info('Associating node to cluster...')
+        master_instance.execute_operation(
+            'cloudify.interfaces.lifecycle.associate',
+            kwargs={'minion_id': machine_id}
+        )
+
+        workctx.logger.info('Kubernetes Worker %s installation script '
+                            'succeeded!', inputs['name'])
+
+    workctx.logger.info('Upscaling Kubernetes cluster succeeded!')
 
 
 def scale_cluster_down(delta):
     master = workctx.get_node('kube_master')
+    # TODO deprecate this! /
     mist_client = connection.MistConnectionClient(properties=master.properties)
     cloud = mist_client.cloud
     master_machine = mist_client.machine
+    # / deprecate
+    # Private IP of Kubernetes Master
     master_ip = master_machine.info['public_ips'][0]
 
     worker_name = inputs.get('worker_name')
+    if not worker_name:
+        raise NonRecoverableError('Kubernetes Worker\'s name is missing')
+
     machines = cloud.machines(search=worker_name)
     if not machines:
-        workctx.logger.info('%s minion node(s) already undeployed', worker_name)
+        workctx.logger.debug('Cannot find node \'%s\'. Already removed? '
+                             'Exiting...', worker_name)
         return
 
-    workctx.logger.info('Terminating worker node(s)')
+    workctx.logger.info('Terminating %d Kubernetes Worker(s)...', len(machines))
     counter = 0
     for m in machines:
         if not m.info['state'] in ('stopped', 'running'):
@@ -239,13 +246,17 @@ def scale_cluster_down(delta):
         worker_priv_ip = m.info['private_ips'][0]
         worker_selfLink = 'ip-' + str(worker_priv_ip).replace('.', '-')
         m.destroy()
-        workctx.logger.info('Removing node reference from the kubernetes cluster')
-        requests.delete("https://%s/api/v1/nodes/%s, auth=HTTPBasicAuth('%s', '%s'), verify=False"
-                        % (master_ip, worker_selfLink,
-                           master.properties['auth_user'], master.properties['auth_pass']))
+        workctx.logger.info('Removing node from the Kubernetes cluster...')
+        requests.delete('https://%s:%s@%s/api/v1/nodes/%s' % \
+                       (master.properties['auth_user'],
+                        master.properties['auth_pass'],
+                        master_ip, worker_selfLink
+                       ), verify=False)
+
         if counter == delta:
             break
-    workctx.logger.info('Downscaling kubernetes cluster succeeded')
+
+    workctx.logger.info('Downscaling Kubernetes cluster succeeded!')
 
 
 scale_cluster()
