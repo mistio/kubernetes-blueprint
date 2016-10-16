a8daa25fe2f3a56a31c3b20826a62cd62c64cfd3
Christos <pollalis.ch@gmail.com>
2016-10-16 22:58:56 +0300

    Fix identation

--- workflows/scale_utils.py
+++ workflows/scale_utils.py
@@ -7,59 +7,61 @@ import random
 
 
 CONSTANTS = {
-    'CREATE_TIMEOUT': 60 * 10                                                         
-    'SCRIPT_TIMEOUT': 60 * 30                                                         
+    'CREATE_TIMEOUT': 60 * 10,
+    'SCRIPT_TIMEOUT': 60 * 30
 }
 
 STORAGE = 'local-storage/local/node-instances/%s_[A-Za-z0-9]*'
 
 
-def LocalStorage(object):
+class LocalStorage(object):
     """
     LocalStorage gives full access to a node instance's properties by reading
     the instance object directly from file
     """
     def __init__(self, node):
-    """
-    Searches for the file in local-storage that corresponds to the node provided
-    """
-    local_storage = self.find_storage(node)
-    instance_file = glob.glob(local_storage)[0]
-    with open(instance_file, 'r') as _instance:
-        instance_from_file = _instance.read()
+        """
+        Searches for the file in local-storage that corresponds to the node
+        provided
+        """
+        local_storage = self.find_storage(node)
+        instance_file = glob.glob(local_storage)[0]
+        with open(instance_file, 'r') as _instance:
+            instance_from_file = _instance.read()
 
-    self.instance_from_file = json.loads(instance_from_file)
+        self.instance_from_file = json.loads(instance_from_file)
 
     @classmethod
     def get(cls, node):
-    """
-    A class method to initiate the LocalStorage with the specified node
-    """
-    node = cls(node)
-    return node
+        """
+        A class method to initiate the LocalStorage with the specified node
+        """
+        node = cls(node)
+        return node
 
     @property
     def runtime_properties(self):
-    """
-    Returns the node instance's runtime properties in a way similiar to `ctx`
-    """
-    return self.instance_from_file['runtime_properties']
+        """
+        Returns the node instance's runtime properties in a way similiar to
+        `ctx`
+        """
+        return self.instance_from_file['runtime_properties']
 
     def find_storage(self, node):
-    """
-    Tries to discover the path of local-storage in order to fetch the
-    required node instance
-    """
-    node_file = os.path.join('/tmp/templates',
-                             'mistio-kubernetes-blueprint-[A-Za-z0-9]*',
-                             STORAGE % node)
-    # TODO: Well, this is weird, but the local-storage exists on a different
-    # path in case a user executes `cfy local` directly from his terminal
-    if not os.path.exists(node_file):
-        if not os.path.exists(os.path.join('..', STORAGE % node)):
-            raise Exception('Failed to locate local-storage in any known path')
-        node_file = os.path.join('..', STORAGE % node)
-    return node_file
+        """
+        Tries to discover the path of local-storage in order to fetch the
+        required node instance
+        """
+        node_file = os.path.join('/tmp/templates',
+                                 'mistio-kubernetes-blueprint-[A-Za-z0-9]*',
+                                 STORAGE % node)
+        # TODO: Well, this is weird, but the local-storage exists on a different
+        # path in case a user executes `cfy local` directly from his terminal
+        if not os.path.exists(node_file):
+            if not os.path.exists(os.path.join('..', STORAGE % node)):
+                raise Exception('Failed to locate local-storage')
+            node_file = os.path.join('..', STORAGE % node)
+        return node_file
 
 
 def random_chars(length=4):
