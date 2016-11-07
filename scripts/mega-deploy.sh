3ad6027e14204089ae7660ab681918331cfd6d6d
commixon <commixon@gmail.com>
2016-11-07 17:02:21 +0200

    Install apt-transport-https for ubuntu

--- scripts/mega-deploy.sh
+++ scripts/mega-deploy.sh
@@ -1114,6 +1114,7 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+apt-get install -y apt-transport-https | echo "Error while installing apt-transport-https. Moving forward"
 apt-get update
 apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni curl
 
