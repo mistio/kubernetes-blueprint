25c3d6549093b7b7518c280af3ecd81fbaaaeb85
Christos Pollalis <pollalis.ch@gmail.com>
2018-04-24 15:05:16 +0300

    14

--- tasks/create.py
+++ tasks/create.py
@@ -45,14 +45,31 @@ def prepare_cloud_init():
         })
 
         #
-        arguments = "-m \'%s\' " % master.runtime_properties['master_ip']
-        arguments += "-t \'%s\' " % master.runtime_properties['master_token']
-        arguments += "-r \'node\'"
+        arguments = "-m '%s' " % master.runtime_properties['master_ip']
+        arguments += "-t '%s' " % master.runtime_properties['master_token']
+        arguments += "-r 'node'"
 
     #
     ctx.logger.debug('Will run mega-deploy.sh with: %s', arguments)
+    data = [
+        ('action', 'cloud_init_finished'),
+        ('machine_name', '$instance-id'),
+    ]
+    data = ['"%s=%s"' % (k, v) for k, v in data]
+    data = '--url-encode ' + ' --url-encode '.join(data)
+    ctx.instance.runtime_properties['cloud_init_data'] = data
+
+    #
+    url = '"%s/api/v1/jobs/%s"' % (ctx.node.properties['mist_config']['mist_uri'],
+                                   ctx.instance.runtime_properties['job_id'])
+    ctx.instance.runtime_properties['cloud_init_url'] = url
+
+    #
+    header = '-H "Authorization: %s"' % ctx.node.properties['mist_config']['mist_token']
+    ctx.instance.runtime_properties['cloud_init_headers'] = header
     ctx.instance.runtime_properties['cloud_init_arguments'] = arguments
 
+    ctx.logger.error('@@@@@@@@@@@@@@@@@@@@@@@ %s', ctx.instance.runtime_properties)
     #
     cloud_init = os.path.join(os.path.dirname(__file__), 'cloud_init.yml')
     ctx.download_resource_and_render(
