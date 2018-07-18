314bcfc0b19599f0e6d4b1f3017da8264a32379b
Christos Pollalis <pollalis.ch@gmail.com>
2018-07-18 20:13:31 +0300

    1

--- tasks/create.py
+++ tasks/create.py
@@ -9,7 +9,6 @@ from plugin.utils import random_string
 from plugin.utils import generate_name
 from plugin.utils import get_stack_name
 
-from plugin.server import get_cloud_id
 from plugin.server import create_machine
 from plugin.connection import MistConnectionClient
 
@@ -136,6 +135,7 @@ if __name__ == '__main__':
     ctx.instance.runtime_properties['machine_name'] = name
 
     #
+    from plugin.server import get_cloud_id
     cloud = conn.get_cloud(get_cloud_id(node_properties))
 
     # Generate cloud-init, if supported.
