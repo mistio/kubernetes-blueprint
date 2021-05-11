b45208cb771377fb6cc429d51bfa3914ef028ffc
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-05-11 12:16:03 +0300

    Add reset kubeadm script

--- scripts/mega-reset.sh
+++ scripts/mega-reset.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
-
-kubeadm reset
+rm -rf /etc/kubernetes/manifests/*.yaml
+rm -rf /var/lib/etcd
+rm -rf /etc/cni/net.d
+kubeadm reset -f
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
